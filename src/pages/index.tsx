112076d6f43de78d1ba41ee0fb584e3f84d7af7f
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-10-19 21:27:15 -0300

    add eslint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const foo = 'bla';
+
   return (
     <div className={styles.container}>
       <Head>
