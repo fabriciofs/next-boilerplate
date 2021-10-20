5738fae8bddecb22e51b8ac8e1e79a0d2590981d
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2021-10-19 21:34:22 -0300

    add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-
-  const foo = 'bla';
+  const foo = 'bla'
 
   return (
     <div className={styles.container}>
